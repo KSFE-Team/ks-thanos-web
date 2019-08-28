3bf711ef7390e77ced894e252413474c45dfb08e
封宇 <fengyu@ksjgs.com>
2019-08-28 11:28:19 +0800

    feat: 初始化Select组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import * as Table from './Table/index';
 import * as Input from './Input/index';
+import * as Select from './Select/index';
 
 export {
     Table,
-    Input
+    Input,
+    Select
 }
 
 
