a213583027f0d0bb79ca7e0fe989f7afda47eed8
陈志强 <chenzhiqiang@ksjgs.com>
2019-08-30 16:08:54 +0800

    feat(新增inputNumber组件):

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,13 @@
 import * as Table from './Table/index';
 import * as Input from './Input/index';
 import * as Select from './Select/index';
+import * as InputNumber from './InputNumber/index';
 
 export {
     Table,
     Input,
-    Select
+    Select,
+    InputNumber
 }
 
 
