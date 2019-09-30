d30521b87d582e615b7535262e1a6dddc0ae569a
caichenyu <caichenyu@ksjgs.com>
2019-09-30 17:05:50 +0800

    build: 发布部署

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
-// const server = process.env.BABEL_ENV === 'production' ? 'http://kaishufe.kaishustory.com' : 'http://10.0.23.7:3000/api';
-const server = 'http://localhost:3012';
-
+const server = process.env.BABEL_ENV === 'production' ? 'http://kaishufe.kaishustory.com' : 'http://10.0.23.7:3000/api';
+// const server = 'http://localhost:3012';
+console.log('server', server);
 export const API = {
     page: {
         query: server + '/api/ks-thanos/v1/page/get',
