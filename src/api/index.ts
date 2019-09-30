b16cafb53d05a0471630f772a689b8afbee3c0ef
caichenyu <caichenyu@ksjgs.com>
2019-09-30 17:31:22 +0800

    feat: 修改API的server地址

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,4 @@
-const server = process.env.BABEL_ENV === 'production' ? 'http://kaishufe.kaishustory.com' : 'http://10.0.23.7:3000/api';
-// const server = 'http://localhost:3012';
-console.log('server', server);
+const server = '/thanos-api';
 export const API = {
     page: {
         query: server + '/api/ks-thanos/v1/page/get',
